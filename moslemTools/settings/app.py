7729478ab37ef21becbbe59f9ec28643b6a42516
abd alrhman mohamed, alcoder <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2025-10-02 05:06:49 +0300

    Update app.py

--- moslemTools/settings/app.py
+++ moslemTools/settings/app.py
@@ -1,7 +1,7 @@
 name="moslem tools"
 appName="moslemTools_GUI"
 appdirname="moslemTools"
-version=2.0
+version=2.1
 description="This program contains all the tools a Muslim needs"
 creater="abd alrhman mohamed alcoder,"
 exit=True
