e1cd05d56fe0d3a950fe090c429440d4141cb7bb
abd alrhman mohamed, alcoder <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2025-10-15 16:25:10 +0300

    Update speak.py

--- moslemTools/guiTools/speak.py
+++ moslemTools/guiTools/speak.py
@@ -1,17 +1,8 @@
-import UniversalSpeech as u_speech
-_speech_engine = None
-try:    
-    _speech_engine = u_speech.UniversalSpeech()        
-    _speech_engine.enable_native_speech(False)        
-    print(f"Screen reader detected: {_speech_engine.engine_used}")
-except Exception as e:    
-    print(f"Failed to initialize UniversalSpeech engine: {e}")
-    _speech_engine = None
-def speak(msg: str, interrupt: bool = True):
-    if _speech_engine:
-        try:            
-            _speech_engine.say(msg, interrupt)
-        except Exception as e:
-            print(f"Error while trying to speak: {e}")
-    else:        
-        print(f"Speech engine not available. Message: '{msg}'")
+import ctypes,os
+dll_path = os.path.join(os.getcwd(), "data", "dlls")
+os.add_dll_directory(dll_path)
+nvda = ctypes.windll.LoadLibrary(os.path.join(dll_path, "nvda_speak.dll"))
+def speak(msg):
+    running = nvda.nvdaController_testIfRunning()            
+    if running != 1:
+        nvda.nvdaController_speakText(msg)
