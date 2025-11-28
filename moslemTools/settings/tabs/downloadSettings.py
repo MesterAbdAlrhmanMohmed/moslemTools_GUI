eac6f4e06eb8ddad253613f83bef0b3ae6b1bfff
abd alrhman mohamed, alcoder <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2025-11-28 10:33:33 +0200

    Update downloadSettings.py

--- moslemTools/settings/tabs/downloadSettings.py
+++ moslemTools/settings/tabs/downloadSettings.py
@@ -2,19 +2,6 @@ import guiTools, gui
 import PyQt6.QtWidgets as qt
 import PyQt6.QtGui as qt1
 import PyQt6.QtCore as qt2
-class DialogThread(qt2.QThread):
-    finished = qt2.pyqtSignal()    
-    def __init__(self, dialog_class, parent, args):
-        super().__init__()
-        self.dialog_class = dialog_class
-        self.parent = parent
-        self.args = args    
-    def run(self):
-        if self.args:
-            self.dialog = self.dialog_class(self.parent, *self.args)
-        else:
-            self.dialog = self.dialog_class(self.parent)
-        self.finished.emit()
 class Download(qt.QDialog):
     def __init__(self):
         super().__init__()
@@ -22,7 +9,7 @@ class Download(qt.QDialog):
         layout = qt.QVBoxLayout(self)
         self.types = guiTools.QListWidget()
         font = qt1.QFont()
-        font.setBold(True)        
+        font.setBold(True)
         self.types.addItems(["كتاب تفسير لتبويبة القرآن الكريم مكتوب", "ترجمة للقرآن الكريم لتبويبة القرآن الكريم مكتوب", "كتاب حديث", "قارئ للقرآن لتبويبة القرآن الكريم مكتوب", "أذكار وأدعية صوتية لتبويبة الأذكار", "الكتب الإسلامية"])
         self.types.setFont(font)
         self.types.clicked.connect(self.onItemClicked)
@@ -36,20 +23,20 @@ class Download(qt.QDialog):
     def onItemClicked(self):
         index = self.types.currentRow()
         if index == 0:
-            self.start_dialog_thread(gui.download.SelectItem, ("all_tafaseers.json", "tafaseer"))
+            self.show_dialog(gui.download.SelectItem, ("all_tafaseers.json", "tafaseer"))
         elif index == 1:
-            self.start_dialog_thread(gui.download.SelectItem, ("all_translater.json", "Quran Translations"))
+            self.show_dialog(gui.download.SelectItem, ("all_translater.json", "Quran Translations"))
         elif index == 2:
-            self.start_dialog_thread(gui.download.SelectItem, ("all_ahadeeth.json", "ahadeeth"))
+            self.show_dialog(gui.download.SelectItem, ("all_ahadeeth.json", "ahadeeth"))
         elif index == 3:
-            self.start_dialog_thread(gui.download.SelectReciter, ())
+            self.show_dialog(gui.download.SelectReciter, ())
         elif index == 4:
-            self.start_dialog_thread(gui.download.SelectAthkar, ())
+            self.show_dialog(gui.download.SelectAthkar, ())
         elif index == 5:
-            self.start_dialog_thread(gui.download.SelectItem, ("all_islamic_books.json", "islamicBooks"))
-    def start_dialog_thread(self, dialog_class, args):
-        self.dialog_thread = DialogThread(dialog_class, self, args)
-        self.dialog_thread.finished.connect(self.show_dialog)
-        self.dialog_thread.start()
-    def show_dialog(self):
-        self.dialog_thread.dialog.exec()
+            self.show_dialog(gui.download.SelectItem, ("all_islamic_books.json", "islamicBooks"))
+    def show_dialog(self, dialog_class, args):
+        if args:
+            dialog = dialog_class(self, *args)
+        else:
+            dialog = dialog_class(self)
+        dialog.show()
