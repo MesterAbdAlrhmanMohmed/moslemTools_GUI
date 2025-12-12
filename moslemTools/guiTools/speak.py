9d4aff6bfa047275b9b76dd10662ee8b046c0333
abd alrhman mohamed, alcoder <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2025-12-12 07:02:09 +0200

    Update speak.py

--- moslemTools/guiTools/speak.py
+++ moslemTools/guiTools/speak.py
@@ -1,8 +1,4 @@
-import ctypes,os
-dll_path = os.path.join(os.getcwd(), "data", "dlls")
-os.add_dll_directory(dll_path)
-nvda = ctypes.windll.LoadLibrary(os.path.join(dll_path, "nvda_speak.dll"))
-def speak(msg):
-    running = nvda.nvdaController_testIfRunning()            
-    if running != 1:
-        nvda.nvdaController_speakText(msg)
+import accessible_output3.outputs
+speaker=accessible_output3.outputs.auto.Auto()
+def speak(text, interrupt=True):
+    speaker.speak(text, interrupt=interrupt)
